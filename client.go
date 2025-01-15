ec5781b36f6f7d3b70ab8ac2870f9d8731d3cb16
Michał Śledź <michalsledz34@gmail.com>
2025-01-15 17:43:50 +0100

    Remove redundant code

--- client.go
+++ client.go
@@ -2,12 +2,10 @@ package whepclient
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/pion/interceptor"
-	"github.com/pion/sdp/v3"
 	"github.com/pion/webrtc/v4"
 )
 
@@ -20,44 +18,16 @@ type Client struct {
 
 func New(url string, pcConfig webrtc.Configuration) (Client, error) {
 	// create peer connection
-
-	// we don't want to use simulcast interceptors
 	interceptorRegistry := &interceptor.Registry{}
 	mediaEngine := &webrtc.MediaEngine{}
 
-	// Setup the codecs you want to use.
-	// We'll only use H264 but you can also define your own
-	if err := mediaEngine.RegisterCodec(webrtc.RTPCodecParameters{
-		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264, ClockRate: 90000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil},
-		PayloadType:        96,
-	}, webrtc.RTPCodecTypeVideo); err != nil {
+	err := mediaEngine.RegisterDefaultCodecs()
+	if err != nil {
 		panic(err)
 	}
 
-	if err := mediaEngine.RegisterCodec(webrtc.RTPCodecParameters{
-		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeOpus, ClockRate: 48000, Channels: 2, SDPFmtpLine: "", RTCPFeedback: nil},
-		PayloadType:        111,
-	}, webrtc.RTPCodecTypeAudio); err != nil {
-		panic(err)
-	}
-
-	if err := webrtc.ConfigureNack(mediaEngine, interceptorRegistry); err != nil {
-		panic(err)
-	}
-
-	if err := webrtc.ConfigureRTCPReports(interceptorRegistry); err != nil {
-		panic(err)
-	}
-
-	if err := webrtc.ConfigureTWCCSender(mediaEngine, interceptorRegistry); err != nil {
-		panic(err)
-	}
-
-	if err := mediaEngine.RegisterHeaderExtension(webrtc.RTPHeaderExtensionCapability{URI: sdp.SDESMidURI}, webrtc.RTPCodecTypeVideo); err != nil {
-		panic(err)
-	}
-
-	if err := mediaEngine.RegisterHeaderExtension(webrtc.RTPHeaderExtensionCapability{URI: sdp.SDESMidURI}, webrtc.RTPCodecTypeAudio); err != nil {
+	err = webrtc.RegisterDefaultInterceptors(mediaEngine, interceptorRegistry)
+	if err != nil {
 		panic(err)
 	}
 
@@ -69,8 +39,8 @@ func New(url string, pcConfig webrtc.Configuration) (Client, error) {
 	}
 
 	client := Client{
-		url:      url,
 		Pc:       pc,
+		url:      url,
 		pcConfig: pcConfig,
 	}
 
@@ -105,11 +75,7 @@ func (client *Client) Connect() {
 	gatherComplete := webrtc.GatheringCompletePromise(client.Pc)
 
 	// Block until ICE Gathering is complete, disabling trickle ICE
-	// we do this because we only can exchange one signaling message
-	// in a production application you should exchange ICE Candidates via OnICECandidate
 	<-gatherComplete
-
-	fmt.Println(client.Pc.LocalDescription().SDP)
 
 	resp, err := http.Post(client.url+"/api/whep", "application/SDP", bytes.NewBufferString(client.Pc.LocalDescription().SDP))
 	if err != nil {
@@ -120,8 +86,6 @@ func (client *Client) Connect() {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println(string(body))
 
 	if err = client.Pc.SetRemoteDescription(webrtc.SessionDescription{Type: webrtc.SDPTypeAnswer, SDP: string(body)}); err != nil {
 		panic(err)
